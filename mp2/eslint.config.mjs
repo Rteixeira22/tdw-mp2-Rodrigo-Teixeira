aec5a23ebe45209d44ac352d3e4879a871283da1
Rteixeira22 <r.teixeira@ua.pt>
2024-11-23 11:05:55 +0000

    EsLint

---
+++ mp2/eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", 
+      },
+    },
+  },
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        jest: true,
+        test: "readonly",
+        expect: "readonly",
+      },
+    },
+  },
+];
