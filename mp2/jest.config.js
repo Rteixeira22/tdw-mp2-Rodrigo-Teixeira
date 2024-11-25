7b8afcd1342cf6a390fa26ed56bd55c9f122f7f8
Rteixeira22 <r.teixeira@ua.pt>
2024-11-25 17:16:27 +0000

    Fix formatting in Jest configuration file

--- mp2/jest.config.js
+++ mp2/jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['js', 'jsx'],
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-  };
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx'],
+};
