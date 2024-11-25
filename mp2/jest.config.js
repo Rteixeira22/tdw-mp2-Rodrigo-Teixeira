f18f12a29223febed7f3a536825615436b7ded20
Rteixeira22 <r.teixeira@ua.pt>
2024-11-25 17:26:08 +0000

    Add coverage configuration to Jest setup

--- mp2/jest.config.js
+++ mp2/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'jsx'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
 };
